88d5b88d579b42b7845d342d1f181b5e902d8e01
Himanshu Garg <himanshugarg5480@gmail.com>
2024-07-25 21:54:44 +0530

    db initiallized

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 // websocket-server/index.js
 import express from "express";
-import { app,server } from "./app.js";
+import { app, server } from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
+
+dotenv.config();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = 3001;
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 8001;
+connectDB().then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}).catch((e)=>{
+  console.log(`error: ${e}`);
+})
